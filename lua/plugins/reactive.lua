2a8a2a0251080e49ef668039ba4f13ad3abda5c9
InvisOn <InvisOn@users.noreply.github.com>
2024-10-13 14:18:54 +1300

    comment description plugin

--- lua/plugins/reactive.lua
+++ lua/plugins/reactive.lua
@@ -1,13 +1,14 @@
+-- visual feedback
 return {
-	"rasulomaroff/reactive.nvim",
-	lazy = false,
-	config = function()
-		require("reactive").setup({
-			builtin = {
-				cursorline = true,
-				cursor = true,
-				modemsg = true,
-			},
-		})
-	end,
+  "rasulomaroff/reactive.nvim",
+  lazy = false,
+  config = function()
+    require("reactive").setup({
+      builtin = {
+        cursorline = true,
+        cursor = true,
+        modemsg = true,
+      },
+    })
+  end,
 }

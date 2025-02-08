b1a7f317a8a41f12346d22cf7dec5558cac93107
InvisOn <InvisOn@users.noreply.github.com>
2025-02-08 20:02:05 +1300

    backup

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,31 +1,28 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",
-  lazy = true,
-  event = {
-    -- if you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    -- e.g. "bufreadpre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    -- refer to `:h file-pattern` for more examples
-    "bufreadpre /home/aj/Vault/*.md",
-    "bufnewfile /home/aj/Vault/*.md",
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-    "nvim-telescope/telescope-fzf-native.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  opts = {
-    ui = {
-      enable = false,
-    },
-    workspaces = {
-      {
-        name = "personal",
-        path = "/home/aj/Vault",
-      },
-    },
+	"epwalsh/obsidian.nvim",
+	version = "*",
+	lazy = true,
+	event = {
+		"bufreadpre " .. vim.fn.expand("~") .. "/Vault/*.md",
+		"bufnewfile " .. vim.fn.expand("~") .. "/Vault/*.md",
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+		"nvim-telescope/telescope-fzf-native.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	opts = {
+		ui = {
+			enable = false,
+		},
+		workspaces = {
+			{
+				name = "personal",
+				path = "/home/aj/Vault",
+			},
+		},
 
-    -- see below for full list of options 👇
-  },
+		-- see below for full list of options 👇
+	},
 }
